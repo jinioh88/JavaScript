4dda1549da185952f9edd63fb32cd9fb209ca7d6
OHSEJIN <jinio88@gmail.com>
2018-06-17 10:33:05 +0900

    프로토타입

--- hello_javascript.js
+++ hello_javascript.js
@@ -3,3 +3,11 @@ for(i=0;i<=10; i++) {
     document.write("Hello javascript!!");
     document.write(("<br/>"));
 }
+
+var foo={
+    name:'foo',
+    age:100
+};
+
+console.log(foo.toString());  // foo의 프로토타입 덕에 toString() 쓸수 있음
+console.dir(foo);
