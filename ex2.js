4b398f58261edf1b32f387397c474662384daf9d
오세진 <jinioh88@gmail.com>
2018-08-05 13:21:49 +0900

    ch2

---
+++ ex2.js
@@ -0,0 +1,9 @@
+function isODD(num) {
+    if(num%2==0)
+        document.write(("작수임"));
+    else
+        document.write("홀수임");
+}
+
+isODD(30);
+isODD(25);
---
+++ ex4.js
@@ -0,0 +1,13 @@
+var car = ["sonata","avante","matiz"];
+
+var revCar = car.reverse();
+
+document.write(revCar);
+
+var person = {
+    firstName : '길동',
+    lastName : '고',
+    country : 'Koea'
+}
+
+document.write(person.firstName+'의 고향은 '+ person.country)
